8ef05d66a3157ed49f1b11f8f37b631098114648
Byron Ellis <byron.ellis@gmail.com>
2018-03-18 14:18:37 -0700

    Spin all the way up to the schema registry and request subjects in testing

--- src/test/java/onememanyus/embedded/EmbeddedKafkaTests.java
+++ src/test/java/onememanyus/embedded/EmbeddedKafkaTests.java
@@ -1,14 +1,24 @@
 package onememanyus.embedded;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
 import org.junit.Test;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class EmbeddedKafkaTests {
 
 	@Test
 	public void testServerStart() throws Exception {
 		EmbeddedZookeeper zk = new EmbeddedZookeeper();
 		EmbeddedKafka     kafka = new EmbeddedKafka(zk);
-		
+		EmbeddedSchemaRegistry registry = new EmbeddedSchemaRegistry(32001,zk.connectString());
+		URL url = new URL(registry.connectString()+"/subjects");
+		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+		log.info("Got response code {}",conn.getResponseCode());
+		conn.disconnect();
+		registry.close();
 		kafka.close();
 		zk.close();
 		
